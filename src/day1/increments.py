364eedb86a774c13da5b0623d68b75e5133e2612
Riccardo Novaglia <rnovaglia@thoughtworks.com>
2021-12-22 15:06:11 +0000

    Day 1, part 2

--- src/day1/increments.py
+++ src/day1/increments.py
@@ -1,14 +1,32 @@
-def get_increments(input_lines):
-    readings = [int(line.strip()) for line in input_lines if line != ""]
+def get_windows(input_readings: [int]) -> [(int, int, int)]:
+    windows = []
+    for index in range(len(input_readings) - 2):
+        windows.append(
+            (
+                input_readings[index],
+                input_readings[index + 1],
+                input_readings[index + 2],
+            )
+        )
+    return windows
 
+
+def count_increments(readings: [int]) -> int:
     increments_count = 0
     for index in range(len(readings) - 1):
         if readings[index] < readings[index + 1]:
             increments_count += 1
-
     return increments_count
+
+
+def count_windows_increments(readings: [int]) -> int:
+    windows = get_windows(readings)
+    window_sums = [x + y + z for (x, y, z) in windows]
+    return count_increments(window_sums)
 
 
 if __name__ == "__main__":
     with open("./input.txt", "r") as file:
-        print(get_increments(file.readlines()))
+        readings = [int(line.strip()) for line in (file.readlines()) if line != ""]
+        print("readings increments: ", count_increments(readings))
+        print("windowed increments: ", count_windows_increments(readings))
--- src/day1/increments_tests.py
+++ src/day1/increments_tests.py
@@ -1,18 +1,57 @@
 from unittest import TestCase
 
-from src.day1.increments import get_increments
+from src.day1.increments import count_increments, count_windows_increments, get_windows
 
 
 class IncrementsTest(TestCase):
     def test_counts_lines_with_number_increments(self):
         cases = [
-            (["1"], 0),
-            (["1", "2"], 1),
-            (["1", "2", "2"], 1),
-            (["1", "2", "1"], 1),
-            (["1", "2", "2", "3"], 2),
-            (["199", "200", "208", "210", "200", "207", "240", "269", "260", "263"], 7),
+            ([1], 0),
+            ([1, 2], 1),
+            ([1, 2, 2], 1),
+            ([1, 2, 1], 1),
+            ([1, 2, 2, 3], 2),
+            ([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 7),
         ]
-        for input_string, expected_increments in cases:
-            with self.subTest(msg=f"{input_string}: {expected_increments}"):
-                self.assertEqual(get_increments(input_string), expected_increments)
+        for input_strings, expected_increments in cases:
+            with self.subTest(msg=f"{input_strings}: {expected_increments}"):
+                self.assertEqual(count_increments(input_strings), expected_increments)
+
+
+class Windows(TestCase):
+    def test_creates_windows_out_of_the_list_of_given_measurements(self):
+        cases = [
+            ([1], []),
+            ([1, 2], []),
+            ([1, 2, 3], [(1, 2, 3)]),
+            ([1, 2, 3, 4], [(1, 2, 3), (2, 3, 4)]),
+            (
+                [199, 200, 208, 210, 200, 207, 240, 269, 260, 263],
+                [
+                    (199, 200, 208),
+                    (200, 208, 210),
+                    (208, 210, 200),
+                    (210, 200, 207),
+                    (200, 207, 240),
+                    (207, 240, 269),
+                    (240, 269, 260),
+                    (269, 260, 263),
+                ],
+            ),
+        ]
+        for input_strings, windows in cases:
+            with self.subTest(msg=f"{input_strings}: {windows}"):
+                self.assertEqual(get_windows(input_strings), windows)
+
+    def test_counts_windows_increments(self):
+        cases = [
+            ([1, 2, 3], 0),
+            ([1, 1, 1, 2, 3], 2),
+            ([1, 1, 1, 2, 3, 4], 3),
+            ([1, 1, 1, 2, 1, 1], 1),
+            ([1, 1, 1, 2, 1, 2], 2),
+            ([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 5),
+        ]
+        for windows, expected_increments in cases:
+            with self.subTest(msg=f"{windows}: {expected_increments}"):
+                self.assertEqual(count_windows_increments(windows), expected_increments)
