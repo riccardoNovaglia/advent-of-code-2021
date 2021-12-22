a3307bd221cbbe087b2676edbf1cba3e28667e32
Riccardo Novaglia <rnovaglia@thoughtworks.com>
2021-12-22 17:02:46 +0000

    Day 2, part 2

--- src/day2/navigation.py
+++ src/day2/navigation.py
@@ -19,8 +19,34 @@ def navigate(directions) -> (int, int):
     return position
 
 
+movements_with_aim = {
+    "forward": lambda position, aim, amount: (
+        (position[0] + amount, position[1] + (amount * aim)),
+        aim,
+    ),
+    "down": lambda position, aim, amount: (position, aim + amount),
+    "up": lambda position, aim, amount: (position, aim - amount),
+}
+
+
+def navigate_with_aim(directions: [str]) -> (int, int):
+    position = 0, 0
+    aim = 0
+    for [direction, amount] in [direction.split(" ") for direction in directions]:
+        position, aim = movements_with_aim[direction](position, aim, int(amount))
+        if position[1] < 0:
+            raise JumpedOutOfWaterException()
+
+    return position
+
+
 if __name__ == "__main__":
     with open("input.txt", "r") as file:
         directions = file.readlines()
         horizontal, depth = navigate(directions)
         print(f"End state is {horizontal},-{depth}, multiplied: {horizontal * depth}")
+        horizontal, depth = navigate_with_aim(directions)
+        print(
+            f"End state with aim is {horizontal},-{depth}, multiplied:"
+            f" {horizontal * depth}"
+        )
--- src/day2/test_navigation.py
+++ src/day2/test_navigation.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from src.day2.navigation import JumpedOutOfWaterException, navigate
+from src.day2.navigation import JumpedOutOfWaterException, navigate, navigate_with_aim
 
 
 class Test(TestCase):
@@ -19,6 +19,24 @@ class Test(TestCase):
         for directions, end_position in cases:
             with self.subTest(msg=f"{directions}: {end_position}"):
                 self.assertEqual(navigate(directions), end_position)
+
+    def test_uses_aim_to_calculate_the_end_coordinates(self):
+        cases = [
+            ([], (0, 0)),
+            (["forward 1"], (1, 0)),
+            (["down 1"], (0, 0)),
+            (["down 1", "forward 1"], (1, 1)),
+            (["down 2", "forward 1"], (1, 2)),
+            (["down 2", "forward 2"], (2, 4)),
+            (["down 2", "forward 1", "up 1", "forward 1"], (2, 3)),
+            (
+                ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"],
+                (15, 60),
+            ),
+        ]
+        for directions, end_position in cases:
+            with self.subTest(msg=f"{directions}: {end_position}"):
+                self.assertEqual(navigate_with_aim(directions), end_position)
 
     def test_throws_an_error_if_we_jump_out_of_the_water(self):
         with self.assertRaises(JumpedOutOfWaterException):
