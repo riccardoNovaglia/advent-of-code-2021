7811d669591347c5145370482c3455a45d1860fc
Riccardo Novaglia <rnovaglia@thoughtworks.com>
2021-12-23 11:07:05 +0000

    Day 3, part 2

--- src/day3/diagnostic.py
+++ src/day3/diagnostic.py
@@ -6,6 +6,8 @@ def diagnose(readings: [str]) -> (str, str):
         stream = [reading[i] for reading in readings]
         if stream.count("0") > len(stream) / 2:
             gamma += "0"
+        elif stream.count("0") == len(stream) / 2:
+            gamma += "1"
         else:
             gamma += "1"
 
@@ -13,12 +15,43 @@ def diagnose(readings: [str]) -> (str, str):
     return gamma, epsilon
 
 
+def oxygen_and_co2_ratings(readings: [str]) -> (str, str):
+    oxygen_candidates = readings.copy()
+    bit_index = 0
+    while len(set(oxygen_candidates)) != 1:
+        gamma, _ = diagnose(oxygen_candidates)
+        popular = gamma[bit_index]
+        oxygen_candidates = list(
+            filter(lambda reading: reading[bit_index] == popular, oxygen_candidates)
+        )
+        bit_index += 1
+
+    co2_candidates = readings.copy()
+    bit_index = 0
+    while len(set(co2_candidates)) != 1:
+        _, epsilon = diagnose(co2_candidates)
+        least_popular = epsilon[bit_index]
+        co2_candidates = list(
+            filter(lambda reading: reading[bit_index] == least_popular, co2_candidates)
+        )
+        bit_index += 1
+        _, epsilon = diagnose(co2_candidates)
+
+    return oxygen_candidates[0], co2_candidates[0]
+
+
 if __name__ == "__main__":
     with open("./input.txt", "r") as file:
-        gamma, epsilon = diagnose(file.readlines())
+        readings = file.readlines()
+        gamma, epsilon = diagnose(readings)
         gamma_decimal = int(gamma, 2)
         epsilon_decimal = int(epsilon, 2)
         print(
             f"Gamma: {gamma} ({gamma_decimal}), Epsilon: {epsilon} ({epsilon_decimal})"
         )
         print(f"Power consumption: {gamma_decimal * epsilon_decimal}")
+        oxygen, co2 = oxygen_and_co2_ratings(readings)
+        oxygen_decimal = int(oxygen, 2)
+        co2_decimal = int(co2, 2)
+        print(f"Oxygen: {oxygen} ({oxygen_decimal}), CO2: {co2} ({co2_decimal})")
+        print(f"Life support rating: {oxygen_decimal * co2_decimal}")
--- src/day3/test_diagnostic.py
+++ src/day3/test_diagnostic.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from src.day3.diagnostic import diagnose
+from src.day3.diagnostic import diagnose, oxygen_and_co2_ratings
 
 
 class Test(TestCase):
@@ -33,5 +33,37 @@ class Test(TestCase):
                 self.assertEqual(diagnose(readings), gamma_epsilon)
 
     def test_does_something_interesting_if_theres_an_even_number_of_readings(self):
-        # TODO?
-        pass
+        cases = [
+            (["0", "0"], ("0", "1")),
+            (["11", "11"], ("11", "00")),
+        ]
+        for readings, gamma_epsilon in cases:
+            with self.subTest(msg=f"{readings}: {gamma_epsilon}"):
+                self.assertEqual(diagnose(readings), gamma_epsilon)
+
+    def test_oxygen_and_co2_ratings(self):
+        cases = [
+            (["0", "1", "1"], ("1", "0")),
+            (["10", "01", "10"], ("10", "01")),
+            (["10", "01", "11"], ("11", "01")),
+            (
+                [
+                    "00100",
+                    "11110",
+                    "10110",
+                    "10111",
+                    "10101",
+                    "01111",
+                    "00111",
+                    "11100",
+                    "10000",
+                    "11001",
+                    "00010",
+                    "01010",
+                ],
+                ("10111", "01010"),
+            ),
+        ]
+        for readings, gamma_epsilon in cases:
+            with self.subTest(msg=f"{readings}: {gamma_epsilon}"):
+                self.assertEqual(oxygen_and_co2_ratings(readings), gamma_epsilon)
