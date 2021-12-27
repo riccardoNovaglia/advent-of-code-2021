99d438707a7df2e2e9f2fc428828d77b80dd4787
Riccardo Novaglia <rnovaglia@thoughtworks.com>
2021-12-27 11:40:15 +0000

    Day 6, part 1, remove botched part 2

--- src/day6/fishes.py
+++ src/day6/fishes.py
@@ -18,14 +18,6 @@ class Fishes:
                 new_generation.append(8)
         self.fishes = new_generation
 
-    def tick_faster(self):
-        spawners_count = self.fishes.count(0)
-        new_gen = [8] * spawners_count
-        reset = [6] * spawners_count
-        to_age = list(map(lambda fish: fish - 1, self.fishes[spawners_count:]))
-        self.fishes = to_age + reset + new_gen
-        self.fishes.sort()
-
     def count_fishes(self) -> int:
         return len(self.fishes)
 
@@ -36,13 +28,7 @@ class Fishes:
 
 if __name__ == "__main__":
     input_string = get_raw_input()
-    # fishes = Fishes.from_string(input_string)
-    # for i in range(80):
-    #     fishes.tick_day()
-    #     print(f"{i}", end=",")
-    # print(f"After 80 days there are now {fishes.count_fishes()} fishes")
-    even_more_fishes = Fishes.from_string(input_string)
-    for i in range(100):
-        even_more_fishes.tick_day()
-        print(f"{i}", end=",")
-    print(f"After 256 days there are now {even_more_fishes.count_fishes()} fishes")
+    fishes = Fishes.from_string(input_string)
+    for i in range(80):
+        fishes.tick_day()
+    print(f"After 80 days there are now {fishes.count_fishes()} fishes")
--- src/day6/test_fishes.py
+++ src/day6/test_fishes.py
@@ -1,4 +1,3 @@
-import time
 from unittest import TestCase
 
 from src.day6.fishes import Fishes
@@ -27,27 +26,6 @@ class TestFishes(TestCase):
         self.assertEqual([6, 8, 0], fishes.fishes)
         fishes.tick_day()
         self.assertEqual([5, 7, 6, 8], fishes.fishes)
-
-    # faster tho
-    def test_fishes_counter_decreases_every_day_faster(self):
-        fishes = Fishes([1])
-        fishes.tick_faster()
-        self.assertEqual([0], fishes.fishes)
-
-    def test_a_fish_will_spawn_another_when_its_counter_reaches_zero_and_resets_faster(
-        self,
-    ):
-        fishes = Fishes([0])
-        fishes.tick_faster()
-        self.assertEqual([6, 8], fishes.fishes)
-        self.assertEqual(2, fishes.count_fishes())
-
-    def test_a_fish_will_spawn_another_but_more_faster(self):
-        fishes = Fishes([0, 1])
-        fishes.tick_faster()
-        self.assertEqual([0, 6, 8], fishes.fishes)
-        fishes.tick_faster()
-        self.assertEqual([5, 6, 7, 8], fishes.fishes)
 
 
 class TestInput(TestCase):
@@ -99,22 +77,3 @@ class TestInput(TestCase):
             actual_fishes,
         )
         self.assertEqual(26, fishes.count_fishes())
-
-    def test_a_quicker_implementation(self):
-        iterations = 150
-
-        fishes = Fishes.from_string("3,4,3,1,2")
-        t = time.process_time()
-        for i in range(iterations):
-            fishes.tick_day()
-        tick_day_elapsed_time = time.process_time() - t
-        print(f"Tick day took {tick_day_elapsed_time}")
-
-        fishes = Fishes.from_string("3,4,3,1,2")
-        t = time.process_time()
-        for i in range(iterations):
-            fishes.tick_faster()
-        tick_faster_elapsed_time = time.process_time() - t
-        print(f"Tick faster took {tick_faster_elapsed_time}")
-
-        self.assertGreater(abs(tick_day_elapsed_time - tick_faster_elapsed_time), 0.5)
