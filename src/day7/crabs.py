ef824b2dedc05221f1d38514829f1284bde60d04
Riccardo Novaglia <rnovaglia@thoughtworks.com>
2021-12-29 18:45:09 +0000

    Day 7, part 2. It works but not very happy with the solution

--- src/day7/crabs.py
+++ src/day7/crabs.py
@@ -1,3 +1,4 @@
+import math
 import statistics
 
 from src.inputs.inputs import get_raw_input
@@ -8,10 +9,37 @@ def get_least_fuel_for_constant_burn(crabs_list):
     return sum([abs(crab_position - best_position) for crab_position in crabs_list])
 
 
+def calculate_fuel_consumption_for_distance(start, end):
+    previous = 0
+    distance = abs(start - end)
+    return sum([i + previous for i in range(1, distance + 1)])
+
+
+def get_least_fuel_for_increasing_burn(crabs_list):
+    mean = statistics.mean(crabs_list)
+    floored_mean = math.floor(mean)
+    ceil_mean = math.ceil(mean)
+
+    floor_mean_consumption = [
+        calculate_fuel_consumption_for_distance(crab_position, floored_mean)
+        for crab_position in crabs_list
+    ]
+    ceil_mean_consumption = [
+        calculate_fuel_consumption_for_distance(crab_position, ceil_mean)
+        for crab_position in crabs_list
+    ]
+
+    return min([sum(floor_mean_consumption), sum(ceil_mean_consumption)])
+
+
 if __name__ == "__main__":
     input_strings = get_raw_input()
     crabs_positions = [int(x) for x in input_strings.split(",")]
     print(
-        f"Median position of the given crabs is "
+        f"Fuel consumed to move to optimal position with constant fuel consumption is "
         f"{get_least_fuel_for_constant_burn(crabs_positions)}"
+    )
+    print(
+        f"Fuel consumed to move to optimal position with increasing fuel consumption "
+        f"is {get_least_fuel_for_increasing_burn(crabs_positions)}"
     )
--- src/day7/test_crabs.py
+++ src/day7/test_crabs.py
@@ -1,11 +1,16 @@
+import math
 import statistics
 from unittest import TestCase
 
-from src.day7.crabs import get_least_fuel_for_constant_burn
+from src.day7.crabs import (
+    calculate_fuel_consumption_for_distance,
+    get_least_fuel_for_constant_burn,
+    get_least_fuel_for_increasing_burn,
+)
 
 
 class CrabsTest(TestCase):
-    def test_the_best_position_for_crabs_to_move_is_the_median(self):
+    def test_the_best_position_for_crabs_to_move_is_the_median_for_constant_fuel(self):
         cases = [
             ([1], 1),
             ([1, 5, 5], 5),
@@ -16,7 +21,9 @@ class CrabsTest(TestCase):
             with self.subTest(msg=f"{example_list}: {expected_answer}"):
                 self.assertEqual(expected_answer, statistics.median(example_list))
 
-    def test_can_get_the_amount_of_fuel_required_to_move_all_crabs_to_the_median(self):
+    def test_can_get_the_amount_of_constant_fuel_required_to_move_all_crabs_to_median(
+        self,
+    ):
         cases = [
             ([1], 0),
             ([1, 5, 5], 4),
@@ -28,3 +35,41 @@ class CrabsTest(TestCase):
                 self.assertEqual(
                     expected_answer, get_least_fuel_for_constant_burn(example_list)
                 )
+
+    def test_best_position_to_move_to_with_least_movement_is_close_to_the_average(self):
+        cases = [
+            ([1], 1),
+            ([1, 4, 4], 3),
+            ([16, 1, 2, 0, 4, 2, 7, 1, 2, 14], 5),
+        ]
+        for example_list, expected_answer in cases:
+            with self.subTest(msg=f"{example_list}: {expected_answer}"):
+                self.assertGreaterEqual(
+                    expected_answer, math.floor(statistics.mean(example_list))
+                )
+                self.assertLessEqual(
+                    expected_answer, math.ceil(statistics.mean(example_list))
+                )
+
+    def test_get_the_amount_of_increasing_fuel_required_to_move_crabs_to_the_average(
+        self,
+    ):
+        cases = [
+            ([1], 0),
+            ([1, 4, 4], 5),
+            ([16, 1, 2, 0, 4, 2, 7, 1, 2, 14], 168),
+        ]
+        for example_list, expected_answer in cases:
+            with self.subTest(msg=f"{example_list}: {expected_answer}"):
+                self.assertEqual(
+                    expected_answer, get_least_fuel_for_increasing_burn(example_list)
+                )
+
+    def test_calculates_the_fuel_consumption_for_a_distance(self):
+        self.assertEqual(0, calculate_fuel_consumption_for_distance(0, 0))
+        self.assertEqual(1, calculate_fuel_consumption_for_distance(0, 1))
+        self.assertEqual(3, calculate_fuel_consumption_for_distance(4, 2))
+        self.assertEqual(6, calculate_fuel_consumption_for_distance(0, 3))
+        self.assertEqual(10, calculate_fuel_consumption_for_distance(4, 8))
+        self.assertEqual(15, calculate_fuel_consumption_for_distance(10, 15))
+        self.assertEqual(21, calculate_fuel_consumption_for_distance(26, 20))
